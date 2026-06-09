3d16c519489a3e9f7475931efb223f0072254dd9
Gajendra Chaudhari <gajendrac@outlook.com>
2026-06-10 04:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (32.87%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -5,10 +5,12 @@ public:
         int j = numbers.size() -1;
 
         while (i<j){
-            if ((numbers[i]+numbers[j]) == target){
+            int sum = numbers[i]+numbers[j];
+
+            if (sum == target){
                 return {i+1,j+1};
             }
-            else if ((numbers[i]+numbers[j]) > target ){
+            else if (sum > target ){
                 j--;
             }
             else i++;
