fa191c112678b3f73b4d6876d3c9571d3b100d62
Gajendra Chaudhari <gajendrac@outlook.com>
2026-06-12 21:46:42 +0530

    Time: 59 ms (10.90%) | Memory: 151.1 MB (12.91%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -28,12 +28,3 @@ public:
         return s.top().second;
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(value);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
